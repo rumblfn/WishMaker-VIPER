68be30eac31ac5d623e16c04ae24d191b3094c08
Shamil <toshamilgis@gmail.com>
2024-12-05 02:40:07 +0300

    enable-user-defaults-for-wishes

--- WishMaker-IOS-Client/WishMaker-IOS-Client/Modules/WishStoring/WishStoringInteractor.swift
+++ WishMaker-IOS-Client/WishMaker-IOS-Client/Modules/WishStoring/WishStoringInteractor.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 protocol WishStoringInteractorInputProtocol: AnyObject {
     func fetchWishes()
     func saveWish(_ wish: String)
@@ -9,18 +11,28 @@ protocol WishStoringInteractorOutputProtocol: AnyObject {
 }
 
 final class WishStoringInteractor: WishStoringInteractorInputProtocol {
-
+    private enum Constants {
+        static let wishesKey = "SavedWishes"
+    }
+    
     weak var presenter: WishStoringInteractorOutputProtocol?
     
     private var wishes: [String] = []
+    private let defaults = UserDefaults.standard
     
     func fetchWishes() {
-        wishes = ["Health and Happiness", "Success and Fulfillment", "Wisdom and Clarity", "Peace and Harmony"]
+        if let savedWishes = defaults.array(forKey: Constants.wishesKey) as? [String] {
+            wishes = savedWishes
+        } else {
+            wishes = []
+        }
+        // wishes = ["Health and Happiness", "Success and Fulfillment", "Wisdom and Clarity", "Peace and Harmony"]
         presenter?.didFetchWishes(wishes)
     }
     
     func saveWish(_ wish: String) {
         wishes.append(wish)
+        defaults.set(wishes, forKey: Constants.wishesKey)
         presenter?.didFetchWishes(wishes)
     }
 }
